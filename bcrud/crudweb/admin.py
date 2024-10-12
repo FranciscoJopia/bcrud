7553113b87fe15aa1cb32b056b62fe153645fa47
Francisco Jopia <francjopia@gmail.com>
2024-10-12 00:17:54 -0300

    Se pudo exitosamente crear un superuser y migrar tabla en models.py y admin.py, falta crear una aplicación atractiva para lo usuarios

--- bcrud/crudweb/admin.py
+++ bcrud/crudweb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dominio
 
-# Register your models here.
+@admin.register(Dominio)
+class DominioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha_creacion', 'fecha_vencimiento','costo_renovacion' )
+    search_field = ('nombre',)
