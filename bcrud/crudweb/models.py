4ab30244df2a749eaacc9cd8bbd93caafd962c1d
Francisco Jopia <francjopia@gmail.com>
2024-10-11 23:46:16 -0300

    Se configura setting para adapta mysql, antes de migrar la base de datos

--- bcrud/crudweb/models.py
+++ bcrud/crudweb/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Dominio(models.Model):
-    nombre = midels.CharField(max_length=100), unique=True)
-    frcha_creacion = models.Date
+    nombre = models.CharField(max_length=100, unique=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_vencimiento = models.DateField()
+    costo_renovacion = models.DecimalField(max_digits=10, decimal_places=2)
+    notas = models.TextField(blank=True)
