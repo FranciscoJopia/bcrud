e9fff80ea3939be1e305f159382b223c6210131f
Francisco Jopia <francjopia@gmail.com>
2024-10-14 01:50:55 -0300

    Se agrega en models.py y admin.py Cliente con sus respectivas configuraciones funcionando correctamente en el admin de Django

--- bcrud/crudweb/admin.py
+++ bcrud/crudweb/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Dominio
+from .models import Cliente, Dominio
+
+@admin.register(Cliente)                # Registra el modelo Cliente.
+class ClienteAsmin(admin.ModelAdmin):   # Puedes Personalizar cómo se verá en el admin.
+    list_display = ('nombre', 'email', 'telefono', 'direccion')
+    search_fields = ('nombre', 'email')
 
 @admin.register(Dominio)
 class DominioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'fecha_creacion', 'fecha_vencimiento','costo_renovacion' )
-    search_field = ('nombre',)
+    list_display = ('nombre', 'cliente', 'fecha_creacion', 'fecha_vencimiento','costo_renovacion' )
+    search_fields = ('nombre','cliente')
--- bcrud/crudweb/models.py
+++ bcrud/crudweb/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=60, unique=True)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, unique=True)
+    direccion = models.CharField(max_length=80, blank=True)
+
+    def __str__(self):
+        return self.nombre
+# Acá termina el modelo de Cliente
 class Dominio(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
+    nombre = models.CharField(max_length=60, unique=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE,related_name='dominios', default=1 )   
+    # Asumiendo que el cliente con ID 1 es el default 
+    # Otra opción es agregar "null=True"
     fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_vencimiento = models.DateField()
     costo_renovacion = models.DecimalField(max_digits=10, decimal_places=2)
-    notas = models.TextField(blank=True)                            
+    notas = models.TextField(blank=True)                          
+
+    def __str__(self):
+        cliente_nombre = self.cliente.nombre if self.cliente else "Sin Cliente" 
+        return f"{self.nombre} - {cliente_nombre}"  
