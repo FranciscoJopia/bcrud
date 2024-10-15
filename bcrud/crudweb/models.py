40580adb85add4e72686943223278917ffe29b23
Francisco Jopia <francjopia@gmail.com>
2024-10-14 22:41:56 -0300

    Se agrega metodos de validación en models.py descargando dependencia de PhoneNumberField y agregandola al archivo en telefono de la class Cliente, También se valida costo_renovación en la class Dominio usando numeros enteros o IntegerField e importando MinValueValidator y MaxValueValidator

--- bcrud/crudweb/models.py
+++ bcrud/crudweb/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 # Create your models here.
 class Cliente(models.Model):
     nombre = models.CharField(max_length=60, unique=True)
     email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=20, unique=True)
+    telefono = PhoneNumberField(     # Usamos PhoneNumberField en lugar de CharField
+        unique=True,
+        region='CL',
+        help_text="Ingresar número fíjo o móvil de forma internacional, Ejemplo:+56XXXXXXXX"
+    )
+    #telefono = models.CharField(max_length=20, unique=True)
     direccion = models.CharField(max_length=80, blank=True)
 
     def __str__(self):
@@ -17,7 +26,12 @@ class Dominio(models.Model):
     # Otra opción es agregar "null=True"
     fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_vencimiento = models.DateField()
-    costo_renovacion = models.DecimalField(max_digits=10, decimal_places=0)
+    costo_renovacion = models.IntegerField( 
+        default=0, 
+        validators=[MinValueValidator(0),           # Valida que el valor sea mayor o igual a 0
+                    MaxValueValidator(20000)]       # Valida el valor máximo es 20000    
+    )
+    #costo_renovacion = models.DecimalField(max_digits=10, decimal_places=0)
     notas = models.TextField(blank=True)                          
 
     def __str__(self):
